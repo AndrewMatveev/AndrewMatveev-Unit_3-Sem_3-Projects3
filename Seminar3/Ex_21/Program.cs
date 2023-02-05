3d91feb3c574c23a08d5ee9a7e29add06a148560
Andrew M <matveev.andrew@internet.ru>
2023-02-05 21:50:58 +0600

    Remaked and added Ex_21 and Ex_22, then commited

--- Seminar3/Ex_21/Program.cs
+++ Seminar3/Ex_21/Program.cs
@@ -4,28 +4,26 @@
 // A (3,6); B (2,1) -> 5,09 
 // A (7,-5); B (1,-1) -> 7,21
 
-int InputNumber(string str)
+double DotsDistance(double ax, double ay, double bx, double by)
 {
-    System.Console.WriteLine(str);
-    return Convert.ToInt32(Console.ReadLine());
+    double Distance = Math.Sqrt(Math.Pow((bx - ax), 2) + Math.Pow((by - ay), 2));
+    Console.WriteLine("Расстояние между точками на плоскости: " + Distance);
+    return Distance;
 } 
 
-InputNumber("")
-
-// System.Console.Write("Введите координаты по X точки А: ");
-// int ax = Convert.ToInt32(System.Console.ReadLine());
-
-// System.Console.Write("Введите координаты по Y точки А: ");
-// int ay = Convert.ToInt32(System.Console.ReadLine());
 
 
-// System.Console.Write("Введите координаты по X точки B: ");
-// int bx = Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите координаты по X точки А: ");
+int ax = Convert.ToInt32(System.Console.ReadLine());
 
-// System.Console.Write("Введите координаты по Y точки B: ");
-// int by = Convert.ToInt32(System.Console.ReadLine());
-
-System.Console.Write(Math.Sqrt(Math.Pow((bx - ax), 2) + Math.Pow((by - ay), 2)));
+System.Console.Write("Введите координаты по Y точки А: ");
+int ay = Convert.ToInt32(System.Console.ReadLine());
 
 
-// Засунуть в формулу
+System.Console.Write("Введите координаты по X точки B: ");
+int bx = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.Write("Введите координаты по Y точки B: ");
+int by = Convert.ToInt32(System.Console.ReadLine());
+
+DotsDistance(ax, ay, bx, by);
--- Seminar3/Ex_22/Program.cs
+++ Seminar3/Ex_22/Program.cs
@@ -1,27 +1,21 @@
-﻿Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
-от 1 до N.
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
 
-5 -> 1, 4, 9, 16, 25.
-2 -> 1,4
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
 
-
-System.Console.WriteLine("Введите число N");
+System.Console.WriteLine("");
+System.Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
+
+
 int b = 1;
 
-while( b <= a)
+
+System.Console.Write("Соответствующие квадраты последовательности от 1 до " + a + ": ");
+while (b <= a)
 {
-    System.Console.WriteLine(Math.Pow(b, 2));
+    System.Console.Write(Math.Pow(b, 2) + " ");
     b++;
 }
-
-
-
-Через функцию
-// void PrintSquares(int x) (
-// for (int i = 1; i <= x; i++)
-// (
-// System.Console.Write(Math.Pow(i, 2)
-// System.Console.WriteLine("Введите число N") int a = Convert.ToInt32(Console.ReadLine())
-// System.Console.WriteLine();
-// PrintSquares(a);
+System.Console.WriteLine("");
