71f4dd0fc5a655be880fd2c73660c83cc565da04
Andrew M <matveev.andrew@internet.ru>
2023-02-05 21:48:55 +0600

    Remaked Ex_18\Program.cs ad commited

--- Seminar3/Ex_18/Program.cs
+++ Seminar3/Ex_18/Program.cs
@@ -1,9 +1,14 @@
-﻿System.Console.WriteLine("Введите четверть");
-int x = Convert.ToInt32(Console.ReadLine());
-Diapazon(x);
-void Diapazon(int a)
-{
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
 
+
+
+
+
+// ____________РЕШЕНИЕ ЧЕРЕЗ ФОРМУЛУ_______________________________________
+
+
+void Diapazon(int a) // формула определение принадлежности к диапазону
+{
     if (a == 1)
     {
         System.Console.WriteLine("Диапазон x ∈ (0;+∞], y ∈ (0;+∞]");
@@ -24,7 +29,9 @@ void Diapazon(int a)
     {
         System.Console.WriteLine("Диапазон не существует");
     }
-
 }
 
-// дорешать
+
+System.Console.Write("Введите четверть: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Diapazon(x);
